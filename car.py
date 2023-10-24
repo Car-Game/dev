6a29210838264943958b95f18ff3e0bccd654428
Pierre_Chambet <pierrechambet@gmail.com>
2023-10-24 19:27:55 +0200

    reorganisation des classes

--- car.py
+++ car.py
@@ -13,86 +13,21 @@ h = CAR.get_height()
 x = 200
 y = 200
 class Car(pygame.sprite.Sprite): #Utilisation de la classe "Sprite" du module "sprite". Car defined by extending Sprite
-    
     def __init__(self):
         super().__init__()
         
-<<<<<<< Updated upstream
-        self.image = pygame.transform.scale(CAR, (w * 0.1, h * 0.1))
-        self.rect = self.image.get_rect(center=(x, y)) 
-        self.clock = pygame.time.Clock()
-        
-=======
         # Initialising car image
         self.image = pygame.transform.scale(CAR, (w * 0.1, h * 0.1))
         self.rect = self.image.get_rect(center=(x, y)) 
-        
-        
-        
-        #self.position = 
->>>>>>> Stashed changes
+
+        self.position = pygame.math.Vector2(x, y)
         self.vel = pygame.math.Vector2(0, 0) 
         self.angle = 0
         
-        self.acceleration = 1
-        self.steering = 1
+        self.acceleration = 0
+        self.steering = 0
         
-    # driving function
-<<<<<<< Updated upstream
-    def drive(self):
+    def update(self, dt):
+        self.vel.x += self.acceleration * dt   #(dv = a * dt)
         
-        dt = self.clock.get_time() / 1000
-        drive_keys = pygame.key.get_pressed()
-        
-        if drive_keys[pygame.K_UP]:
-            self.acceleration = self.acceleration + 1 * dt
-            self.vel.x = self.vel.x + self.acceleration * dt
-            
-        elif drive_keys[pygame.K_DOWN]:
-            self.acceleration = self.acceleration - 1 * dt
-            self.vel.x = self.vel.x - self.acceleration * dt  
-=======
-    #def drive(self):
-        
-        #dt = main.clock.get_time() / 1000
- 
->>>>>>> Stashed changes
-            
-    # steering function
-    def steer(self):
-        steer_keys = pygame.key.get_pressed()
-        
-        # test déplacement / rotation avec K_RIGHT
-        
-<<<<<<< Updated upstream
-        if steer_keys[pygame.K_RIGHT]:
-            #self.angle -= self.angle_vel
-            self.rotated_image = pygame.transform.rotate(self.image, self.angle)
-=======
-        #if steer_keys[pygame.K_RIGHT]:
-            #self.angle -= self.angle_vel
-            #self.rotated_image = pygame.transform.rotate(self.image, self.angle)
->>>>>>> Stashed changes
-            #new_rect = self.rotated_image.get_rect(center=(x,y))
-            
-            #self.rect.y += self.vel[0]
-            #self.vel[1] = self.vel[1] + self.angle_vel
-            #self.vel.rotate_ip(self.angle_vel)
-
-        # test vitesse / vecteur avec K_LEFT
-<<<<<<< Updated upstream
-        elif steer_keys[pygame.K_LEFT]:
-            #self.angle += self.angle_vel
-            self.rotated_image = pygame.transform.rotate(self.image, self.angle)
-=======
-        #elif steer_keys[pygame.K_LEFT]:
-            #self.angle += self.angle_vel
-            #self.rotated_image = pygame.transform.rotate(self.image, self.angle)
->>>>>>> Stashed changes
-            #new_rect = self.rotated_image.get_rect(center=(x,y))
-            #self.rect.y -= self.vel[0]
-            
-    # update driving and steering (which will depend on the user inputs)
-    def update(self):
-        self.drive()
-        #self.steer()
+        self.position += self.vel.rotate(-self.angle) * dt  #(dx = v * dt)
--- main.py
+++ main.py
@@ -2,68 +2,68 @@ import pygame
 import os
 import car
 
-# Initialising all pygame modules
-pygame.init()
 
-
-LARGEUR = 1000 #dimension de l'image circuit
-HAUTEUR = 600
-
-screen = pygame.display.set_mode((LARGEUR, HAUTEUR)) #Afficher l'ecran de jeu
-CIRCUIT = pygame.image.load("./circuit.jpeg")
-
-# Resising image so it fits right
-CIRCUIT = pygame.transform.scale(CIRCUIT, (1000, 600))
-
-car = car.Car()
-
-clock = pygame.time.Clock()
-dt = clock.tick(60) / 1000
-
-def user_inputs():
-    drive_keys = pygame.key.get_pressed()
+class Game():
+    def __init__(self):
+        pygame.init() 
+        pygame.display.set_caption("Car Race")
+        
+        LARGEUR = 1000
+        HAUTEUR = 600
+        
+        self.screen = pygame.display.set_mode((LARGEUR, HAUTEUR))
+        self.clock = pygame.time.Clock()
+        self.ticks = 60
+        
+        CIRCUIT = pygame.image.load("./circuit.jpeg")
+        CIRCUIT = pygame.transform.scale(CIRCUIT, (1000, 600))
+        
+        self.running = True
+        
+    def run(self):
+        CAR = pygame.image.load(os.path.join(os.getcwd(), "./car.png"))
+        car_image = pygame.transform.scale(CAR, (w * 0.1, h * 0.1))
+        w = CAR.get_width()
+        h = CAR.get_height()
+        car = car.Car()
     
-    if drive_keys[pygame.K_UP]:
-        car.acceleration += dt
-        car.vel.x += car.acceleration * dt
-        print(car.vel)
-        
-    elif drive_keys[pygame.K_DOWN]:
-        car.acceleration -= dt
-        car.vel.x -= car.acceleration * dt 
-
-# Run until user quits window
-running = True
-while running:
-    
-
-
-    for event in pygame.event.get():
-        
-        if event.type == pygame.QUIT:
-            running = False
-        
-        elif event.type == pygame.KEYDOWN:
+        while self.running:
             
-            if event.key == pygame.K_ESCAPE:
-                running = False
-    
-    # Clear the screen to erase the drag of the car 
-    screen.fill((0, 0, 0))
-
-    # display race track on the screen with .blit()
-    screen.blit(CIRCUIT, (0, 0))
-
-    # updating the position of the car
-    car.update()
-    
-    # displays the car on the track
-    
-    screen.blit(car.image, car.rect)
-    
-    # update the display
-    pygame.display.update()
-    pygame.display.flip()
-        
-# Quit pygame when done playing
-pygame.quit()
+            #définir un temps infinitésimal
+            dt = self.clock.get_time() / 1000
+            
+            #définir une condition de sortie
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+            
+            #définition des commandes
+            key_pressed = pygame.key.get_pressed()
+            
+            if key_pressed[pygame.K_UP]:
+                car.acceleration += dt
+                #car.vel.x += car.acceleration * dt  #(dv = a * dt)
+                
+            elif key_pressed[pygame.K_DOWN]:
+                car.acceleration -= dt
+                #car.vel.x -= car.acceleration * dt  #(dv = a * dt)
+                
+            
+            car.update(dt)
+            
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(Game.CIRCUIT, (0, 0))
+            
+            rotated_image = pygame.transform.rotate(car_image, car.angle)
+            rect = rotated_image.get_rect()
+            
+            self.screen.blit(car.self.image, car.self.rect)
+            pygame.display.flip()
+            
+            self.clock.tick(self.ticks)
+            
+        pygame.quit()
